174c0ab239cf8d46cab74a5cda1e5d3e038ebf8e
Brenocastrocardoso <brenocastrocardoso@gmail.com>
2017-09-29 19:07:46 -0300

    29/09

--- jogador.py
+++ jogador.py
@@ -2,11 +2,12 @@
 import spade
 import time
 
-class mapa(object):
+class mapa():
     """docstring for mapa"""
     def __init__(self, ordem):
         self.lugarx = 4
         self.lugary = 4
+        self.caminho = ""
         self.matriz = [[0 for j in range(ordem)] for i in range(ordem)]
         for i in range(0,ordem):
             for j in range(0, ordem):
@@ -17,15 +18,19 @@ class mapa(object):
 
     def proximo(self, cima, direita, baixo, esquerda):
         lista = [999,999,999,999]
-
+        print "proximo(" + str(cima) + "," + str(direita) + "," + str(baixo) + "," + str(esquerda) + ")" 
         if cima==1 :
             lista[0] = self.matriz[self.lugarx][self.lugary+1]
+            print "lista[0]: " + str(lista[0])
         if direita==1 :
             lista[1] = self.matriz[self.lugarx+1][self.lugary]
+            print "lista[1]: " + str(lista[1])
         if baixo==1 :
-            lista[2] = self.matriz[self.lugarx+1][self.lugary]
+            lista[2] = self.matriz[self.lugarx][self.lugary-1]
+            print "lista[2]: " + str(lista[2])
         if esquerda==1 :
-            lista[3] = self.matriz[self.lugarx+1][self.lugary]
+            lista[3] = self.matriz[self.lugarx-1][self.lugary]
+            print "lista[3]: " + str(lista[3])
         aux = 0
         min = lista[0]
         if min > lista[1]:
@@ -37,17 +42,48 @@ class mapa(object):
         if min > lista[3]:
             min = lista[3]
             aux = 3
+        print "aux: " + str(aux)
         return aux
 
+    def andar(self, direcao):
+        #sleep(1)
+        print "self.lugarx: ",
+        print self.lugarx
+        print "self.lugary: ",
+        print self.lugary
+        aux = self.matriz[self.lugarx][self.lugary] 
+        print aux
+        self.matriz[self.lugarx][self.lugary] = aux + 1
+        if direcao=="esq" :
+            self.lugarx = self.lugarx - 1
+        if direcao=="dir" :
+            self.lugarx = self.lugarx + 1
+        if direcao=="cima" :
+            self.lugary = self.lugary +1
+        if direcao=="baixo" :
+            self.lugary = self.lugary -1
+        print self.lugarx
+        print self.lugary
+        
+        self.caminho = self.caminho + direcao + "|"
+        print self.caminho
 
+    def getCaminho(self):
+        print "getcaminho"
+        return self.caminho
 
+m = mapa(10)
 
 class Resolve_Labirinto(spade.Agent.Agent):
     class solve(spade.Behaviour.Behaviour):
         def _process(self):
-            m = mapa(10)
+            global m
             msg = self._receive(True)
             content  = msg.getContent()
+            performative = msg.getPerformative()
+            print "mensagem recebida"
+            print content
+            print performative
             if content == 'Start':
                 msg = spade.ACLMessage.ACLMessage()
                 msg.setPerformative("request")
@@ -72,15 +108,25 @@ class Resolve_Labirinto(spade.Agent.Agent):
                 self.myAgent.send(msg)
                 print "Requisicao de sucessores enviada"
             elif content == 'true':
+                print "entrou no true"
+                caminho = m.getCaminho()
+                print caminho
+                caminho = caminho[:-1]
+                print caminho
+                msg = spade.ACLMessage.ACLMessage()
+                msg.setPerformative("propose")
+                msg.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
+                msg.setContent(caminho)
+                self.myAgent.send(msg)
                 print "Objetivo encontrado, enviando proposta de caminho"
                 #salva o caminho e o numero de passos dados
                 #se for a melhor resposta, manda proposta de caminho
             elif (content[0] == '[' and content[-1] == ']'):
 
                 cima =  int(content[1])
-                esquerda = int(content[4])
+                direita = int(content[4])
                 baixo = int(content[7])
-                direita = int(content[10])
+                esquerda = int(content[10])
 
                 print cima
                 print esquerda
@@ -105,7 +151,9 @@ class Resolve_Labirinto(spade.Agent.Agent):
                     msg.setContent("baixo")
                 self.myAgent.send(msg)
                 print "acao enviada"
-            elif content=="done":
+            elif (content=="esq" or content=="dir" or content=="cima" or content=="baixo") or performative=="done":
+                print "entrou"
+                m.andar(content)
                 msg = spade.ACLMessage.ACLMessage()
                 msg.setPerformative("request")
                 msg.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
@@ -115,10 +163,11 @@ class Resolve_Labirinto(spade.Agent.Agent):
                 #manda proposta de caminho
             elif content=="failure":
                 print "Failed to perform action"
+            elif performative == "accept-proposal":
+                print "SUCESSO"
             else:
                 #toma decisao de caminhada pelo labirinto
                 print "else: ", content
-
 
     def _setup(self):
 
@@ -126,6 +175,7 @@ class Resolve_Labirinto(spade.Agent.Agent):
         #template1.setPerformative("request")
         t1 = spade.Behaviour.MessageTemplate(template1)
         self.addBehaviour(self.solve(),t1)
+
 
 
 
