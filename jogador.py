c7c03c930fd726c1eb816d4105a29405816e6e6b
Brenocastrocardoso <brenocastrocardoso@gmail.com>
2017-10-01 14:09:30 -0300

    jogado.py concertado

--- jogador.py
+++ jogador.py
@@ -3,7 +3,8 @@ import spade
 import time
 from random import randint
 
-class mapa(object):
+
+class mapa():
     """docstring for mapa"""
     def __init__(self, ordem):
         self.lugarx = 4
@@ -18,12 +19,14 @@ class mapa(object):
                 print str(i) + " "+ str(j)'''
 
     def proximo(self, cima, direita, baixo, esquerda):
+        
         l = [cima, direita, baixo, esquerda]
         aux = randint(0, 3)
         while (l[aux] == 0):
             aux = randint(0,3)
         print aux
         return aux
+        
         '''
         lista = [999,999,999,999]
         print "proximo(" + str(cima) + "," + str(direita) + "," + str(baixo) + "," + str(esquerda) + ")" 
@@ -50,8 +53,32 @@ class mapa(object):
         if min > lista[3]:
             min = lista[3]
             aux = 3
+        print "aux: " + str(aux)'''
         return aux
 
+    def andar(self, direcao):
+        #sleep(1)
+        '''
+        print "self.lugarx: ",
+        print self.lugarx
+        print "self.lugary: ",
+        print self.lugary
+        aux = self.matriz[self.lugarx][self.lugary] 
+        print aux
+        self.matriz[self.lugarx][self.lugary] = aux + 1
+        if direcao=="esq" :
+            self.lugarx = self.lugarx - 1
+        if direcao=="dir" :
+            self.lugarx = self.lugarx + 1
+        if direcao=="cima" :
+            self.lugary = self.lugary +1
+        if direcao=="baixo" :
+            self.lugary = self.lugary -1
+        print self.lugarx
+        print self.lugary
+        '''
+        self.caminho = self.caminho + direcao + "|"
+        print self.caminho
 
     def getCaminho(self):
         print "getcaminho"
@@ -134,7 +161,10 @@ class Resolve_Labirinto(spade.Agent.Agent):
                     msg.setContent("esq")
                 self.myAgent.send(msg)
                 print "acao enviada"
-            elif content=="done":
+            elif (content=="esq" or content=="dir" or content=="cima" or content=="baixo") or performative=="done":
+                print "entrou"
+                print content
+                m.andar(content)
                 msg = spade.ACLMessage.ACLMessage()
                 msg.setPerformative("request")
                 msg.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
